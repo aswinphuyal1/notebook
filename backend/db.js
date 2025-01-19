791ddcc444b3cad07117536c1eef3919a1345716
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-01-19 21:22:03 +0545

    11.01

--- backend/db.js
+++ backend/db.js
@@ -5,6 +5,7 @@ const ConnectToMongo = () => {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }).then(() => {
+    
     console.log('Connected to MongoDB');
   }).catch((error) => {
     console.error('Error connecting to MongoDB:', error);
