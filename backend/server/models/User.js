be9eb537337bc0ab3c2cf2e738ca8c640fa10686
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-01-24 19:47:50 +0545

    dackend connected

---
+++ backend/server/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+   
+      name: {
+        type: String,
+        required: true
+      },
+      Email: {
+        type: String,
+        required: true
+        },
+        password: {
+            type: String,
+            required: true
+          },
+          phone: {
+            type: String,
+            required: true
+          },
+          address: {
+            type: String,
+            required: true
+          },
+          city: {
+            type: String,
+            required: true
+          },
+          state: {
+            type: String,
+            required: true
+          },
+          zip: {
+            type: String,
+            required: true
+          },
+          country: {
+            type: String,
+            required: true }
+ 
+});
+
+mongoose.exports = mongoose.model('User', userSchema);
