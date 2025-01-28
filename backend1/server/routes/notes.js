62d278b39860384d4a6a6609a714ca64788e18aa
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-01-29 00:05:20 +0545

    harry ko tyo hlayo backend

--- backend1/server/routes/notes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Define your routes for notes here
-router.get('/', (req, res) => {
-    res.send('Notes page');
-});
-
-module.exports = router;
