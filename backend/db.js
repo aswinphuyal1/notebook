235af2d5783d4dab76340304d36befbb948bcab0
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-01-29 00:32:31 +0545

    error esolved npm run dev

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+//const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURI = "mongodb://localhost:27017/"
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
         console.log("Connected to Mongo Successfully");
-    })
-}
+    });
+};
 
 module.exports = connectToMongo;
