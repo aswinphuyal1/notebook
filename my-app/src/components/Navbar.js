0b12ddfde0c3a7650d762cb576c85b6bb303b6ef
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-02-05 18:32:37 +0545

    loaction path is used in home and about buttion to be marked which buttion is poressed

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -19,10 +19,10 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
+              <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About</Link>
             </li>
           </ul>
           <form className="d-flex" role="search">
