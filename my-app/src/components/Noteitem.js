9c3c59a55aec8ab5cd64f84699b3daf31255efbd
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-02-10 23:59:26 +0545

    bootstarap added

--- my-app/src/components/Noteitem.js
+++ my-app/src/components/Noteitem.js
@@ -1,23 +1,18 @@
-
 //rafce 
 import React from 'react'
+// Add this line to import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Noteitem = (props) => {
  const    {note}=props
   return (
-    <div>
-      {note.title}
-      {note.description}
-
-      <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
+    <div className="col-md-3">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{note.title}</h5>
+          <p className="card-text">{note.description}</p>
+        </div>
+      </div>
     </div>
   )
 }
