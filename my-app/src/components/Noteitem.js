da861dccf359ddd15d362aab155977d6f0c2fc25
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-02-10 23:24:15 +0545

    doccumentd fromatted

--- my-app/src/components/Noteitem.js
+++ my-app/src/components/Noteitem.js
@@ -1,25 +1,27 @@
-
-//rafce 
-import React from 'react'
+//rafce
+import React from "react";
 
 const Noteitem = (props) => {
- const    {note}=props
+  const { note } = props;
   return (
     <div>
       {note.title}
       {note.description}
-      
-      <div class="card" >
 
-  <div class="card-body">
-    <h5 class="card-title">title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Noteitem
+export default Noteitem;
